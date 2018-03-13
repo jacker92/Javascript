c76dd5b061bf5dd56c776af8d93f513e5a3e69e2
Jaakko Lahtinen <32266520+jacker92@users.noreply.github.com>
2018-03-13 09:00:46 +0200

    Added marker list in HTML and auto-zoom between markers

--- google.js
+++ google.js
@@ -2,27 +2,28 @@ var returnable = [];
 var locations = [];
 var index = 0;
 
+
 function handleButton(addMarker) {
   var address = document.getElementById('address').value;
   var city = document.getElementById('city').value;
 
-  if(city.trim().length==0 && address.trim().length==0) {
+  if (city.trim().length == 0 && address.trim().length == 0) {
     document.getElementById('error').style.visibility = "visible";
     document.getElementById('error').innerHTML = "<h1>Please enter a valid address!</h1>"
     return;
   }
   getCoordinates(address, city, addMarker);
-  document.getElementById('error').style.visibility = "hidden";
+  document.getElementById('error').innerHTML = "";
 }
 
 function getCoordinates(address, city, addMarker) {
   var test = "https://maps.googleapis.com/maps/api/geocode/json?address=" + address + " " + city + "A&key=AIzaSyD_Yt0izIctnEEiG8VOoiQ_hj79kkOzrcI";
   returnable = [];
   $.getJSON(test, function(data) {
-  returnable[0] = data["results"][0]["geometry"]["location"]["lat"];
-  returnable[1] = data["results"][0]["geometry"]["location"]["lng"];
-  initMap(returnable, addMarker);
-   });
+    returnable[0] = data["results"][0]["geometry"]["location"]["lat"];
+    returnable[1] = data["results"][0]["geometry"]["location"]["lng"];
+    initMap(returnable, addMarker);
+  });
 
 }
 
@@ -43,47 +44,83 @@ var getDistance = function(p1, p2) {
 };
 
 function calculateDistance() {
-document.getElementById('error').innerHTML = "<h1>Straight line distance between two markers is approx. " + (getDistance(locations[0], locations[1])/1000).toFixed(2) + " kilometers </h1>";
-document.getElementById('error').style.visibility = "visible";
-locations = [];
-document.getElementById('calculateDistance').style.visibility = "hidden";
-index = 0;
+  document.getElementById('error').innerHTML = "<h1>Straight line distance between two markers is approx. " + (getDistance(locations[0], locations[1]) / 1000).toFixed(2) + " kilometers </h1>";
+  document.getElementById('error').style.visibility = "visible";
+  locations = [];
+  document.getElementById('calculateDistance').style.visibility = "hidden";
+  index = 0;
+  initMarkerList();
 }
 
 
 function initMap(coords, addMarker) {
 
-  if(coords==null) return;
+  if (coords == null) return;
 
-  //console.log(coords[]);
   console.log(coords);
-  if(coords.length == 0) {
-  document.getElementById('addressnotfound').style.visibility = "visible";
+  if (coords.length == 0) {
+    document.getElementById('addressnotfound').style.visibility = "visible";
   }
-  var uluru = {lat: coords[0], lng: coords[1]};
+  var uluru = {
+    lat: coords[0],
+    lng: coords[1]
+  };
 
-  if(addMarker) {
-  locations[index] =  uluru;
-  index++;
-}
+  if (addMarker) {
+    locations[index] = uluru;
+    index++;
+  }
 
-if(locations.length == 2) {
+  if (locations.length == 2) {
     document.getElementById('calculateDistance').style.visibility = "visible";
-} else {
+  } else {
     document.getElementById('calculateDistance').style.visibility = "hidden";
-}
+  }
 
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 17,
     center: uluru
   });
   var markers = locations.map(function(location, i) {
-       return new google.maps.Marker({
-         position: location,
-       });
-     });
+    return new google.maps.Marker({
+      position: location,
+    });
+  });
 
+  if (addMarker) {
+    var bounds = new google.maps.LatLngBounds();
+    for (var i = 0; i < locations.length; i++) {
+      bounds.extend(locations[i]);
+    }
+    map.fitBounds(bounds);
+  }
 
-var markerCluster = new MarkerClusterer(map, markers,
-           {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
-     }
+  var markerCluster = new MarkerClusterer(map, markers, {
+    imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
+  });
+  initMarkerList();
+
+}
+
+function initMarkerList() {
+
+  var list = '<ul><li><a class="ui-all" tabindex="-1">';
+
+  for (var i = 0; i < locations.length; i++) {
+    list += locations[i].lat + ",";
+    list += locations[i].lng + '</a></li>';
+
+    if (i != locations.length - 1) {
+      list += '<li>';
+    }
+  }
+
+  list += '</li></ul>';
+  console.log(list);
+  if (locations.length > 0) {
+    document.getElementById("listOfMarkers").innerHTML = "<h2>List of markers:</h2> \n" + list;
+  } else {
+    document.getElementById("listOfMarkers").innerHTML = "";
+  }
+
+}
